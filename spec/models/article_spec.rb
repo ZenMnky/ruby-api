9496b1759e441aab7b291ddf9bb029ff35deb4cc
Justin Hager <developerjustinhager@pm.me>
2021-04-05 13:47:30 -0700

    adds additional validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,15 +8,40 @@ RSpec.describe Article, type: :model do
     
     it 'tests that factory is valid' do
         # article = Article.create({title: 'Sample article', content: 'Sample content' })
-        article = build(:article)
         expect(article).to be_valid # article-valid? == true
+        article.save!
+        another_article = build(:article)
+        expect(another_article).to be_valid
       end
 
-      it 'has an invalid title' do
-        article.title = ''
-        expect(article).not_to be_valid
-        expect(article.errors[:title]).to include("can't be blank")
-      end
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+
+    it 'has invalid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+
+    it 'validates the uniquness of the slug' do 
+      article1 = create(:article)
+      expect(article1).to be_valid
+      article2 = build(:article, slug: article1.slug)
+      expect(article2).not_to be_valid
+      expect(article2.errors[:slug]).to include('has already been taken')
+    end
+
   end
 
   
