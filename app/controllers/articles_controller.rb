fbf0f0e11f9241bd688de376b44a4acad91e5585
Justin Hager <developerjustinhager@pm.me>
2021-04-05 15:21:13 -0700

    serialize articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,11 @@
 class ArticlesController < ApplicationController
+    def index
+        articles = Article.all
+        render json: serializer.new(articles), status: :ok
+    end
+
+    def serializer
+        ArticleSerializer
+    end
 
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe '/articles routes' do
             expect(get '/articles').to route_to('articles#index')
             expect(get '/articles?page[number]=3').to route_to('articles#index', page: {'number' => '3' })
         end
-        
+    end
+
+    it 'rotues to articles#show' do 
+        expect(get('/articles/1')).to route_to('articles#show', id: '1')
     end
 end
