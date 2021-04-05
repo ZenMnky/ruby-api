cfbc5b108166dc933fc2741207e2f68abe72714b
Justin Hager <developerjustinhager@pm.me>
2021-04-05 13:30:29 -0700

    add tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests article object' do
-    # article = Article.create({title: 'Sample article', content: 'Sample content' })
-    article = create(:article)
-    expect(article.title).to eq('Sample article')
+  # hash prefix used to denote testing instances
+  # dot prefix used to denote testing class
+  describe '#validations' do
+    let(:article) { build(:article) }  
+    
+    it 'tests that factory is valid' do
+        # article = Article.create({title: 'Sample article', content: 'Sample content' })
+        article = build(:article)
+        expect(article).to be_valid # article-valid? == true
+      end
+
+      it 'has an invalid title' do
+        article.title = ''
+        expect(article).not_to be_valid
+        expect(article.errors[:title]).to include("can't be blank")
+      end
   end
+
+  
 end
