b69903974ae2045982dfe14b10f63e06e58fa5a1
Justin Hager <developerjustinhager@pm.me>
2021-04-14 12:01:06 -0700

    extracts serializer and options to Paginable module

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,12 @@
 class ArticlesController < ApplicationController
   include Paginable
-  
+
   def index
     paginated = paginate(Article.recent)
-
-    options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
-    render json: serializer.new(paginated.items, options), status: :ok
+    render_collection(paginated)
   end
 
   def serializer
     ArticleSerializer
   end
-
-  
 end
--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -19,38 +19,39 @@ RSpec.describe ArticlesController do
         expect(expected[:id]).to eq(article.id.to_s)
         expect(expected[:type]).to eq('articles')
         expect(expected[:attributes]).to eq(
-            title: article.title,
-            content: article.content,
-            slug: article.slug
+          title: article.title,
+          content: article.content,
+          slug: article.slug
         )
       end
     end
 
     it 'returns articles in the proper order' do
-        older_article = create(:article, created_at: 1.hour.ago)
-        recent_article = create(:article)
-        get '/articles'
-        ids = json_data.map {|item| item[:id].to_i}
-        expect(ids).to( 
-            eq([recent_article.id, older_article.id])
-        )
+      older_article = create(:article, created_at: 1.hour.ago)
+      recent_article = create(:article)
+      get '/articles'
+      ids = json_data.map { |item| item[:id].to_i }
+      expect(ids).to(eq([recent_article.id, older_article.id]))
     end
 
-    it 'paginates results' do 
+    it 'paginates results' do
       article1, article2, article3 = create_list(:article, 3)
-      get '/articles', params: {page: { number: 2, size: 1}}
+      get '/articles', params: { page: { number: 2, size: 1 } }
       expect(json_data.length).to eq(1)
       expect(json_data.first[:id]).to eq(article2.id.to_s)
     end
 
-    it 'contains pagination links in the response' do 
+    it 'contains pagination links in the response' do
       article1, article2, article3 = create_list(:article, 3)
-      get '/articles', params: {page: { number: 2, size: 1}}
+      get '/articles', params: { page: { number: 2, size: 1 } }
       expect(json[:links].length).to eq(5)
       expect(json[:links].keys).to contain_exactly(
-        :first, :prev, :next, :last, :self
+        :first,
+        :prev,
+        :next,
+        :last,
+        :self
       )
     end
-
   end
 end
