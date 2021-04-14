c41c573b6441ba0d36456a9d173d0ae91dd11f62
Justin Hager <developerjustinhager@pm.me>
2021-04-14 11:40:43 -0700

    adds JSOM-Paginator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,23 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.recent
-    render json: serializer.new(articles), status: :ok
+    paginated = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url
+    )
+    render json: serializer.new(paginated.items), status: :ok
   end
 
   def serializer
     ArticleSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
